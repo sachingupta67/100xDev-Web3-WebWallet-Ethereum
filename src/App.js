bfc61bd84c0f8dd372d70c15cdf8bf56357bda1c
Sachin Gupta <42978777+sachingupta67@users.noreply.github.com>
2024-08-11 20:15:19 +0530

    Web Wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [mnemonic, setMnemonic] = useState("");
+  const [wallets, setWallet] = useState([]);
+
+  const generateMnemonic = async () => {
+    const randomMnemonic = ethers.Wallet.createRandom().mnemonic.phrase;
+    setMnemonic(randomMnemonic);
+  };
+
+  const addWallet = async () => {
+    if (mnemonic) {
+      const wallet = ethers.Wallet.fromPhrase(mnemonic);
+      setWallet((prev)=>[...prev, wallet]);
+      setMnemonic(()=>"");
+
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Wallet | Ethereum</h1>
+      <button onClick={generateMnemonic}>
+        {mnemonic ? "Generate Other Mnemonic" : "Generate Mnemonic"}
+      </button>
+      {mnemonic && <p>Mnemonic: {mnemonic}</p>}
+      <button onClick={addWallet} disabled={!mnemonic}>
+        Add Wallet 
+      </button>
+      <h2>Wallets</h2>
+      <ul>
+        {wallets.map((wallet, index) => (
+          <li key={index}>
+            <p>Address: {wallet.address}</p>
+            <p>Public Key: {wallet.publicKey}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
-
+};
 export default App;
